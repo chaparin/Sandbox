1630ae708ee2c0e12932f5c129215c20002a6239
chaparin <chaparin@gmail.com>
2025-01-16 20:03:43 -0500

    Testing versioning

--- poem.py
+++ poem.py
@@ -3,3 +3,5 @@ pluralAdjective = input("Please enter a plural adjective: ")
 
 print(pluralNoun.capitalize(), "are red, violets are blue")
 print("Monty Python is", pluralAdjective.lower() + ", woo hoo!")
+
+# Test comment
